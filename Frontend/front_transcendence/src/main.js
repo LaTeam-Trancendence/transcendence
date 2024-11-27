1cfe19ac5d481a3562ae45131d1fa0d8d6c9eaec
Gabarnou <146858389+Hewya@users.noreply.github.com>
2024-11-27 18:26:05 +0100

    Deleting submodule

--- Frontend/front_transcendence/src/main.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import "./assets/styles/tailwind.css"
-import i18n from './i18n'
-import store from './store'
-import router from './router'
-
-createApp(App).use(i18n).use(store).use(router).mount('#app')
-
