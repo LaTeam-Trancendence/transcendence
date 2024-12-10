000c28b9302f21a996bc313298869b0b7a17925d
Gabarnou <146858389+Hewya@users.noreply.github.com>
2024-12-10 18:44:20 +0100

    Removing too much mkdir for logs and a mnt/log in mnt/logs

--- Backend_API/scripts/cron.sh
+++ Backend_API/scripts/cron.sh
@@ -4,7 +4,7 @@ echo "Starting monthly GDPR anonymization task..."
 
 # Exécute la commande Django pour anonymiser les utilisateurs
 # python Backend_API/back/transcendance/manage.py anonymize_users
-echo "Starting cron..." >> /mnt/log/api/cron.log
+echo "Starting cron..." >> /mnt/logs/api/cron.log
 crond -l 2
 
 echo "Monthly GDPR anonymization task completed."
