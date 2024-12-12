804a3b861650c5ae3dfec8f2d7efe3de36f03847
Gabarnou <146858389+Hewya@users.noreply.github.com>
2024-12-12 11:37:52 +0100

    Services are running and Dashboards are created

--- Backend_API/Back/test.py
+++
@@ -1,4 +0,0 @@
-# mot de passe creation de joueurs
-
-# vani0, 1, ..., n
-# Vanitestmdp!
--- Backend_API/friends/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from friends.views import AddFriendsView, FriendListView
-
-
-urlpatterns = [
-    path('addFriends', AddFriendsView.as_view(), name='addFriends'),
-    path('friendsList', FriendListView.as_view(), name='friendsList')    
-]
